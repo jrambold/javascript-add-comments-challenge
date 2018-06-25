219fbef13c2ff541f5f688fe2012ce62a2970018
jrambold <jrambold@gmail.com>
2018-06-25 06:11:43 -0600

    challenge complete

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
   // add your code here
+  $("form").hide();
+
+  $('#new_comment_button').on("click", function() {
+    $("form").show();
+    $("#new_comment_button").hide();
+  });
+
+  $('#form_submit').on("click", function(event) {
+    event.preventDefault();
+    $("form").hide();
+    $("#new_comment_button").show();
+    let comment = $('input[name="comment"]').val();
+    let user = $('input[name="user"]').val();
+
+    if (comment) {
+      $(`<li>${comment}<span class="author">${user}</span></li>`).appendTo("#comment_list");
+    }
+  });
 })
